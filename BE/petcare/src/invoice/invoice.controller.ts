085ce489a8a52459610fa6c7fcffc0a8c3bda472
Long <nguyenlong14052004@gmail.com>
2026-03-17 10:36:49 +0700

    feature: code create invoice api

--- BE/petcare/src/invoice/invoice.controller.ts
+++ BE/petcare/src/invoice/invoice.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { InvoiceService } from './invoice.service';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
+import { CreateInvoiceDTO } from './dtos/create-invoice.dto';
 
 @Controller('invoice')
-export class InvoiceController {}
+export class InvoiceController {
+  constructor(private readonly invoiceService: InvoiceService) {}
+
+  @Post('')
+  @ApiOperation({ summary: 'Tạo mới hoá đơn' })
+  @ApiBody({
+    type: CreateInvoiceDTO,
+  })
+  createInvoice(@Body() createDTO: CreateInvoiceDTO) {
+    return this.invoiceService.createInvoice(createDTO);
+  }
+}
